7a15a9b08cd5d42877bdf22cb982fb91edea8770
NischayChauhan <nischay0007.nsc@gmail.com>
2019-03-05 22:05:29 +0530

    Task_00_NischayChauhan

---
+++ task_00_NischayChauhan.py
@@ -0,0 +1,45 @@
+import numpy as np
+def add(x, y):
+   return x + y
+def subtract(x, y):
+   return x - y
+def multiply(x, y):
+   return x * y
+def divide(x, y):
+   return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Exponential")
+print("6.Log")
+
+choice = input("Enter choice(1/2/3/4/5/6):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2," =", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2," =", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2," =", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2," =", divide(num1,num2))
+
+elif choice == '5':
+   print("exp(",num1,") =", np.exp(num1))
+   print("exp(",num2,") =", np.exp(num2))
+
+elif choice == '6':
+   print("log(",num1,") =", np.log(num1))
+   print("log(",num2,") =", np.log(num2))
+
+else:
+   print("Invalid input")
